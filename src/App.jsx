9082d243d193981a989db2e38fc31b4cdbe49765
Martha Watson <marthacdenzer@gmail.com>
2024-11-04 23:04:37 -0700

    added more project structure and code to project and header. Combined header with navigation under new name, header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,4 @@
-import { useState } from 'react'
-import Project from './components/Project'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    <Project />
-    </>
-  )
-}
-
-export default App
+import Project from './components/Project';
+import 'bootstrap/dist/css/bootstrap.min.css';
+const App = () => <Project />;
+export default App;
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,26 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import Project from './pages/Project';
+import Portfolio from './pages/Portfolio';
 import Resume from './pages/Resume';
-
+import Header from './Header';
+import Footer from './Footer';
+// import '../assets/style/style.css';
+export default function Project() {
+    return (
+        <>
+            <Header />
+            <main className="container mt-5">
+                <Routes>
+                    <Route path='/' element={<About />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/portfolio" element={<Portfolio />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/resume" element={<Resume />} />
+                    <Route path="*" element={<Navigate to="/about" replace />} />
+                </Routes>
+            </main>
+            <Footer />
+        </>
+    );
+}
