97ff7cfe03fd4fd89bed1ed1c2e4edc8f2eeb45f
Martha Watson <marthacdenzer@gmail.com>
2024-11-04 23:11:28 -0700

    bommented out Navigation, because for a simpler approach, I am combining it with header

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,42 +1,42 @@
-import { Link, useLocation } from 'react-router-dom';
+// import { Link, useLocation } from 'react-router-dom';
 
-export default function Navigation() {
-    const location = useLocation();
-    return (
-        <ul>
+// export default function Navigation() {
+//     const location = useLocation();
+//     return (
+//         <ul>
 
-            <li>
-                <Link to="/about"
-                    onClick={() => console.log('About link clicked')}
-                    className={location.pathname === '/about' ? 'nav-link active' : 'nav-link'}
-                >
-                    About
-                </Link>
-            </li>
-            <li>
-                <Link to="/Portfolio"
-                    onClick={() => console.log('Portfolio link clicked')}
-                    className={location.pathname === '/Portfolio' ? 'nav-link active' : 'nav-link'}
-                >
-                    About
-                </Link>
-            </li>
-            <li>
-                <Link to="/Contact"
-                    onClick={() => console.log('Contact link clicked')}
-                    className={location.pathname === '/Contact' ? 'nav-link active' : 'nav-link'}
-                >
-                    About
-                </Link>
-            </li>
-            <li>
-                <Link to="/Resume"
-                    onClick={() => console.log('Resume link clicked')}
-                    className={location.pathname === '/about' ? 'nav-link active' : 'nav-link'}
-                >
-                    About
-                </Link>
-            </li>
-        </ul>
-    )
-}
+//             <li>
+//                 <Link to="/about"
+//                     onClick={() => console.log('About link clicked')}
+//                     className={location.pathname === '/about' ? 'nav-link active' : 'nav-link'}
+//                 >
+//                     About
+//                 </Link>
+//             </li>
+//             <li>
+//                 <Link to="/Portfolio"
+//                     onClick={() => console.log('Portfolio link clicked')}
+//                     className={location.pathname === '/Portfolio' ? 'nav-link active' : 'nav-link'}
+//                 >
+//                     About
+//                 </Link>
+//             </li>
+//             <li>
+//                 <Link to="/Contact"
+//                     onClick={() => console.log('Contact link clicked')}
+//                     className={location.pathname === '/Contact' ? 'nav-link active' : 'nav-link'}
+//                 >
+//                     About
+//                 </Link>
+//             </li>
+//             <li>
+//                 <Link to="/Resume"
+//                     onClick={() => console.log('Resume link clicked')}
+//                     className={location.pathname === '/about' ? 'nav-link active' : 'nav-link'}
+//                 >
+//                     About
+//                 </Link>
+//             </li>
+//         </ul>
+//     )
+// }
